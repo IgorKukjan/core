00f7530fba1ced1704845e6cc6944d8804ca261b
Igor Kukyan <kukyan@irc.beltelecom.by>
2024-10-08 11:32:53 +0300

    first

---
+++ src/test/java/by/javaguru/payments/ws/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package by.javaguru.payments.ws.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
